89c52ed4e9bd401c8be9bb97228db22ee1b2e90c
Liron Yahdav <lyahdav@users.noreply.github.com>
2022-09-13 16:51:16 -0700

    NSTextView-NSTextField-NSButton-focus-testing

--- TestMacApp/ViewController.m
+++ TestMacApp/ViewController.m
@@ -9,12 +9,31 @@
 
 @implementation ViewController
 
+- (void)addButton:(CGFloat)y {
+  NSButton *btn = [[NSButton alloc] initWithFrame:CGRectMake(50, y, 300, 50)];
+  btn.title = @"Button";
+  [self.view addSubview:btn];
+}
+
 - (void)viewDidLoad {
-  NSTextView *tv = [[NSTextView alloc] initWithFrame:CGRectMake(50, 50, 300, 100)];
+  [self addButton:0];
+  
+  NSTextView *tv = [[NSTextView alloc] initWithFrame:CGRectMake(50, 60, 300, 40)];
+  [tv.textStorage appendAttributedString:[[NSAttributedString alloc] initWithString:@"NSTextView"]];
   tv.maxSize = CGSizeMake(300, 100);
   tv.selectable = YES;
-  tv.editable = YES;
+  tv.editable = NO;
   [self.view addSubview:tv];
+
+
+  NSTextField *tf = [[NSTextField alloc] initWithFrame:CGRectMake(50, 100, 300, 40)];
+  tf.selectable = YES;
+  tf.editable = NO;
+  tf.stringValue = @"NSTextField";
+  [self.view addSubview:tf];
+
+  
+  [self addButton:200];
 }
 
 @end
